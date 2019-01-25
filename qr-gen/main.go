589614e852f4c106c59f0ec852d6ee315278b4fb
David Guan <davidguan@canva.com>
2019-01-25 13:35:48 +1100

    support new style

--- qr-gen/main.go
+++ qr-gen/main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"image/color"
+	"io/ioutil"
 	"log"
 	"os"
 
 	qrcode "github.com/skip2/go-qrcode"
 )
 
-func encode(url string) string {
+func encode(url string, newStyle bool) string {
 	var png []byte
-	png, err := qrcode.Encode(url, qrcode.Medium, 256)
+	var err error
+	if newStyle {
+		err = qrcode.WriteColorFile(url, qrcode.Medium, 256, color.White, color.RGBA{0, 199, 206, 255}, "qr.png")
+		if err != nil {
+			log.Fatal(err)
+		}
+		png, err = ioutil.ReadFile("qr.png")
+	} else {
+		png, err = qrcode.Encode(url, qrcode.Medium, 256)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	sEnc := base64.StdEncoding.EncodeToString([]byte(png))
 	return fmt.Sprintf("data:image/png;base64,%s", string(sEnc))
 }
@@ -23,6 +36,6 @@ func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Need tell me what to encode")
 	}
-	b64 := encode(os.Args[1])
+	b64 := encode(os.Args[1], len(os.Args) > 2)
 	fmt.Print(b64)
 }
